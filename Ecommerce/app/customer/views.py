8a32049fe7c63ce8f5e9320169b6ae8122033071
Sreenath k k <sreenathkk00@gmail.com>
2024-03-27 09:42:02 +0530

    login

--- Ecommerce/app/customer/views.py
+++ Ecommerce/app/customer/views.py
@@ -5,9 +5,10 @@ from django.contrib import messages
 from .models import customers
 # Create your views here.
 def account(request):
-    error_massage=None
+    context={}
+
     if request.POST and 'register' in request.POST:
-        print(request.POST)
+        context['register']=True
         
         username=request.POST.get('username')
         email = request.POST.get('email')
@@ -17,6 +18,7 @@ def account(request):
         
         if password != repeat_password:
             error_massage="password do not match"
+            messages.error(request,error_massage)
         else:
             try:
 
@@ -32,8 +34,27 @@ def account(request):
                 phone=phone
                 )
                 return redirect('index')
+            
             except:
                 error_massage='Username Alredy Taken' 
+                messages.error(request,error_massage)
+
+     # login request           
+    if request.POST and 'login' in request.POST:
+        context['register']=False
+        username=request.POST.get('username')
+        password= request.POST.get('password')
+        user=authenticate(
+            username=username,
+            password=password
+            )         
+        if user:
+            login(request,user)
+            return redirect('index')  
+        else:
+            error_massage='Incorrect Username Or password' 
+            messages.error(request,error_massage)
 
             
-    return render(request,'account.html',{'error_massage':error_massage})
+            
+    return render(request,'account.html',context)
